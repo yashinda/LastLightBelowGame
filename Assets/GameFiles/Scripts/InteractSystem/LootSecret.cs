ea00182a2e76ba57c0ff41a427480379ca7593b9
yashinda <104185861+yashinda@users.noreply.github.com>
2026-03-26 16:45:46 +0400

    New enemy and change secrets scripts

--- Assets/GameFiles/Scripts/InteractSystem/LootSecret.cs
+++ Assets/GameFiles/Scripts/InteractSystem/LootSecret.cs
@@ -1,12 +1,26 @@
 using UnityEngine;
 
+public enum BuffCharacteristic
+{
+    Health,
+    Psi,
+    Armor,
+    Revolver,
+    Shotgun,
+    Rifle
+}
+
 public class LootSecret : MonoBehaviour, IInteractable
 {
+    [SerializeField] private GameObject levelController;
     public StatisticsLevel statistic;
     public SvetlesContainer svetlesContainer;
     [SerializeField] private int svetlesCount;
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private AudioClip audioClip;
+    [SerializeField] private Transform player;
+    [SerializeField] private BuffCharacteristic characteristic;
+    [SerializeField] private int buffCount;
 
     public void Interact()
     {
@@ -15,6 +29,28 @@ public class LootSecret : MonoBehaviour, IInteractable
         audioSource.PlayOneShot(audioClip);
 
         Destroy(gameObject);
+
+        switch (characteristic)
+        {
+            case BuffCharacteristic.Health:
+                player.GetComponent<PlayerHealth>().IncreaseMaxHP(buffCount);
+                break;
+            case BuffCharacteristic.Psi:
+                levelController.GetComponent<PsySystem>().IncreaseMaxPsi(buffCount);
+                break;
+            case BuffCharacteristic.Armor:
+                player.GetComponent<PlayerHealth>().GetArmor(buffCount);
+                break;
+            case BuffCharacteristic.Revolver:
+                player.GetComponentInChildren<Revolver>().IncreaseDamage(buffCount);
+                break;
+            case BuffCharacteristic.Shotgun:
+                player.GetComponentInChildren<Shotgun>().IncreaseDamage(buffCount);
+                break;
+            case BuffCharacteristic.Rifle:
+                player.GetComponentInChildren<Rifle>().IncreaseDamage(buffCount);
+                break;
+        }
     }
 
     public string GetInteractionDescription() => "взять секрет";

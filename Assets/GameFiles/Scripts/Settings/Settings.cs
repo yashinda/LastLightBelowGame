89cb016746fa83a7330249ef9ffd27e527107868
yashinda <104185861+yashinda@users.noreply.github.com>
2026-05-05 16:54:41 +0400

    Changed script settings

--- Assets/GameFiles/Scripts/Settings/Settings.cs
+++ Assets/GameFiles/Scripts/Settings/Settings.cs
@@ -1,20 +1,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class Settings : MonoBehaviour
 {
+    [Header("UI Elements")]
     public TMP_Dropdown dropdownResolution;
+
     public Slider sliderMusic;
     public Slider sliderSounds;
+    public Slider sliderSensitivity;
+
     public TMP_Text textValueMusic;
     public TMP_Text textValueSounds;
-    public AudioSource musicSource;
+    public TMP_Text textSensitivity;
 
-    private List<Vector2Int> allowedResol = new List<Vector2Int>()
+    [Header("Audio")]
+    public AudioMixer mixerSettings;
+
+    [Header("Audio Settings")]
+    public float minMusicValue = -47.0f;
+    public float maxMusicValue = 0.0f;
+
+    public float minSoundValue = -47.0f;
+    public float maxSoundValue = 0.0f;
+
+    [Header("Sensitivity Settings")]
+    public float minSensitivity = 1.0f;
+    public float maxSensitivity = 6.0f;
+
+    public float sensitivity;
+    public float musicVolume;
+    public float soundVolume;
+
+    private List<Vector2Int> allowedResol = new()
     {
         new Vector2Int(1280, 720),
         new Vector2Int(1650, 1050),
@@ -23,52 +45,151 @@ public class Settings : MonoBehaviour
         new Vector2Int(3840, 2160)
     };
 
-    public float maxMusicValue = 1.0f;
-    public float minMusicValue = 0.0f;
-    public float maxSoundValue = 1.0f;
-    public float minSoundValue = 0.0f;
-
-    private void Update()
-    {
-        float valueMusic = sliderMusic.value * 100f;
-        float valueSounds = sliderSounds.value * 100f;
-        textValueMusic.text = Mathf.FloorToInt(valueMusic).ToString();
-        textValueSounds.text = Mathf.FloorToInt(valueSounds).ToString();
-    }
-
     private void Start()
     {
+        SetupResolution();
+        SetupMusicSlider();
+        SetupSoundSlider();
+        SetupSensitivitySlider();
+        LoadGraphicSettings();
+    }
+
+    private void SetupResolution()
+    {
         dropdownResolution.ClearOptions();
-        List<string> optionsResolution = allowedResol.Select(res => $"{res.x} x {res.y}").ToList();
-        dropdownResolution.AddOptions(optionsResolution);
-        dropdownResolution.onValueChanged.AddListener(OnResolutionChanged);
+
+        List<string> options = allowedResol.Select(r => $"{r.x} x {r.y}").ToList();
+
+        dropdownResolution.AddOptions(options);
 
         int savedIndex = PlayerPrefs.GetInt("ResolutionIndex", allowedResol.Count - 1);
+
         dropdownResolution.value = savedIndex;
         dropdownResolution.RefreshShownValue();
-        OnResolutionChanged(savedIndex);
+
+        ApplyResolution(savedIndex);
+
+        dropdownResolution.onValueChanged.AddListener(OnResolutionChanged);
+    }
+
+    private void SetupMusicSlider()
+    {
+        sliderMusic.minValue = minMusicValue;
+        sliderMusic.maxValue = maxMusicValue;
+
+        float savedMusicVolume = PlayerPrefs.GetFloat("MusicVolume", 0.0f);
+
+        sliderMusic.value = savedMusicVolume;
+
+        ChangeMusicValue(savedMusicVolume);
+
+        sliderMusic.onValueChanged.AddListener(ChangeMusicValue);
+    }
+
+    private void SetupSoundSlider()
+    {
+        sliderSounds.minValue = minSoundValue;
+        sliderSounds.maxValue = maxSoundValue;
+
+        float savedSoundVolume = PlayerPrefs.GetFloat("SoundVolume", 0.0f);
+
+        sliderSounds.value = savedSoundVolume;
+
+        ChangeSoundValue(savedSoundVolume);
+
+        sliderSounds.onValueChanged.AddListener(ChangeSoundValue);
+    }
+
+    private void SetupSensitivitySlider()
+    {
+        sliderSensitivity.minValue = minSensitivity;
+        sliderSensitivity.maxValue = maxSensitivity;
+
+        float savedSensitivity = PlayerPrefs.GetFloat("SensitivityValue", 3.0f);
+
+        sliderSensitivity.value = savedSensitivity;
+
+        ChangeSensitivity(savedSensitivity);
+
+        sliderSensitivity.onValueChanged.AddListener(ChangeSensitivity);
+    }
+
+    private void LoadGraphicSettings()
+    {
+        int graphicLevel = PlayerPrefs.GetInt("GraphicLevel", QualitySettings.GetQualityLevel());
+
+        QualitySettings.SetQualityLevel(graphicLevel);
+    }
+
+    private void ApplyResolution(int index)
+    {
+        Vector2Int selected = allowedResol[index];
+
+        Screen.SetResolution(selected.x, selected.y, Screen.fullScreen);
+    }
+
+    private void UpdateTexts()
+    {
+        float musicPercent = (sliderMusic.value - minMusicValue) / -minMusicValue * 100f;
+        float soundPercent = (sliderSounds.value - minSoundValue) / -minSoundValue * 100f;
+
+        textValueMusic.text = Mathf.RoundToInt(musicPercent).ToString();
+
+        textValueSounds.text = Mathf.RoundToInt(soundPercent).ToString();
+
+        textSensitivity.text = Mathf.RoundToInt(sliderSensitivity.value).ToString();
+    }
+
+    public void ChangeMusicValue(float sliderValue)
+    {
+        musicVolume = sliderValue;
+
+        mixerSettings.SetFloat("VolumeMusic", musicVolume);
+
+        PlayerPrefs.SetFloat("MusicVolume", musicVolume);
+        PlayerPrefs.Save();
+
+        UpdateTexts();
+    }
+
+    public void ChangeSoundValue(float sliderValue)
+    {
+        soundVolume = sliderValue;
+
+        mixerSettings.SetFloat("VolumeSound", soundVolume);
+
+        PlayerPrefs.SetFloat("SoundVolume", soundVolume);
+        PlayerPrefs.Save();
+
+        UpdateTexts();
+    }
+
+    public void ChangeSensitivity(float value)
+    {
+        sensitivity = value;
+
+        PlayerPrefs.SetFloat("SensitivityValue", value);
+        PlayerPrefs.Save();
+
+        UpdateTexts();
     }
 
     public void OnResolutionChanged(int index)
     {
-        Vector2Int selectedRes = allowedResol[index];
-        Screen.SetResolution(selectedRes.x, selectedRes.y, Screen.fullScreen);
+        ApplyResolution(index);
 
         PlayerPrefs.SetInt("ResolutionIndex", index);
         PlayerPrefs.Save();
-    }
-
-    public void ChangeMusicValue()
-    {
-        musicSource.volume = sliderMusic.value;
     }
 
     public void SetGraphicLevel(int index)
     {
         QualitySettings.SetQualityLevel(index);
         QualitySettings.vSyncCount = 0;
+
         PlayerPrefs.SetInt("GraphicLevel", index);
         PlayerPrefs.Save();
+
         Debug.Log($"Set Graphic Level: {QualitySettings.names[QualitySettings.GetQualityLevel()]}");
     }
 }
